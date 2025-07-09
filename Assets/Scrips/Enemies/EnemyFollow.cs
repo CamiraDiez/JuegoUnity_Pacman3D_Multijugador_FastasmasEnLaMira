cca4e3605b15f66e75ffbbcf8c952f814ea0ccd5
CamiraDiez <programaciondatoscami@gmail.com>
2025-07-09 15:33:33 -0500

    Reaorganiacion de carpetas y preparacion para modo multijugador
    -Reorganiacion de carpetas del proyecto para estructurar mejor la logica multijugador
    -Terminados los scripts iniciales borradores de "PlayerMovement" y "EnemyFollow"
    -Falta adaptar estos scripts para funcionar correctamente en modo multijugador (lo hare mas tarde)
    
    Proximos pasos:
    -Modificar "PlayerMovement"para movimientos multijugador

--- Assets/Scrips/Enemies/EnemyFollow.cs
+++ Assets/Scrips/Enemies/EnemyFollow.cs
@@ -10,7 +10,7 @@ public class EnemyFollow : MonoBehaviour
 
     public Transform target;
     Vector3 dest;    //aqui ya esta privado, cordenadas x, y, z
-    NavMeshAgent agente;          //moverlo automaticamente en el laberinto 
+    private NavMeshAgent agente;          //moverlo automaticamente en el laberinto 
 
     public LayerMask LayerPlayer;    //para hacer la deteccion de solo el jugador por parte del enemigo
     public float rangoEnemigo;      //Distancia donde el enemigo puede detectar al jugador ...los fantasmas siguen al jugador solo si esta cerca
@@ -19,7 +19,8 @@ public class EnemyFollow : MonoBehaviour
     // Se ejecuta una sola vez cuando el enemigo aparece en escena
     void Start()
     {
-        agente = GetComponent<NavMesh>();     //usamos la variable "agente" para decirle al enemigo a donde moverse, cual es la velocidad....
+        agente = GetComponent<NavMeshAgent
+        >();     //usamos la variable "agente" para decirle al enemigo a donde moverse, cual es la velocidad....
     
         dest = agente.destination;            //variable= dest la posicion actual hacia donde se esta moviendo el enemigo
         
@@ -28,6 +29,25 @@ public class EnemyFollow : MonoBehaviour
     // 
     void Update()
     {
+        enemigoActivo = Physics.CheckSphere(transform.position, rangoEnemigo, LayerPlayer);   //metodo checkspphere
+
+        if(enemigoActivo == true)      //entro en la espera? si si entonces..
+        {
+            if(Vector3.Distance (dest, target.position) > -1f)
+            {
+                dest = target.position;       //El enemigo nos empiea a perseguir porque esta buscando nuestra posicion
+                agente. destination = dest;
+            }
+
+
+        }
         
     }
+     //vamos a ver la esfera para ver como ocurre todo-Gizmos 
+        private void OnDrawGimos()
+        {
+            Gizmos.color = new Color(1f, 0.5f,0f);    //Naranja intenso
+            Gizmos.DrawWireSphere(transform. position, rangoEnemigo);
+        }
+
 }
--- Assets/Scrips/Player/PlayerMovement.cs
+++ Assets/Scrips/Player/PlayerMovement.cs
@@ -17,14 +17,14 @@ public class PlayerMovement : MonoBehaviour
     // 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = -Input.GetAxis("Horizontal");
+        float verticalInput = -Input.GetAxis("Vertical");
 
         //Movimiento
-        Vector3 move =  new Vector3(horizontalInput, 0f, verticalInput);   //momiento en x, y, z
+        Vector3 move =  new Vector3(horizontalInput, 0f, verticalInput);   //momiento en x, z
         move.Normalize();
         transform.position =  transform.position + move * speed * Time.deltaTime;   //deltaTime ayuda al movimiento se vea bien
-
+ 
         if (move != Vector3.zero)   //nos dice si el objeto se esta moviendo o nop
         {
             transform.rotation =  Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), rotationSpeed * Time.deltaTime);//quaterion es la funcion para los angulos
