c4060c449e96391a2bf5c263f3ce2aa578637fd9
CamiraDiez <programaciondatoscami@gmail.com>
2025-07-10 13:58:40 -0500

    Implementacion inicial de movimiento de jugador y logica de enemigos con NavMesh
    -Se añadio el script PlayerMovement.cs para controlar el movimiento del jugador con rotacion suave]
    -Se creo el script EnemyFollow.cs para permitir que los fantasmas sigan al jugador usando NavMeshAgent y deteccion por rango.
    -Se configuro agentes NavMesh para permitir que los fantasmas sigan al jugador usando NavMeshAgent y deteccion por rango]
    -Se configuro angentes NavMesh personaliados (EnemyAgent) y bake del laberinto con NavMeshSurFace
    -Se asignaron referencias y capas necesarias (Player, Default) para deteccion de colisiones
    -Se creo comentarios en el codigo para mis compañeros
    -Se probo 3 enemigos persiguiendo al jugador

--- Assets/Scrips/Enemies/EnemyFollow.cs
+++ Assets/Scrips/Enemies/EnemyFollow.cs
@@ -1,42 +1,64 @@
+
 using System.Collections;
 using System.Collections.Generic;   //para los ciclos for 
 using UnityEngine;
 using UnityEngine.AI;     //Libreria para usar la ia para los enemigos (importa el sistema NavMesh)
+
+//using Photon.Pun;       //CAMI BORRA EL COMENTARIO
 
 
 [RequireComponent (typeof (NavMeshAgent))]       //vamos a usar NavMesh y lo agrege por defecto
 public class EnemyFollow : MonoBehaviour
 {
 
-    public Transform target;
-    Vector3 dest;    //aqui ya esta privado, cordenadas x, y, z
-    private NavMeshAgent agente;          //moverlo automaticamente en el laberinto 
+    // --- REFERENCIAS  ---
+    public Transform playerTarget;        //El jugador al que el enemigo va a seguir
+    //Vector3 dest;    //aqui ya esta privado, cordenadas x, y, z
+    private NavMeshAgent agent;          //moverlo automaticamente en el laberinto 
 
-    public LayerMask LayerPlayer;    //para hacer la deteccion de solo el jugador por parte del enemigo
-    public float rangoEnemigo;      //Distancia donde el enemigo puede detectar al jugador ...los fantasmas siguen al jugador solo si esta cerca
-    bool enemigoActivo;             //vamos a guardar si el enemigo ya esta persiguiendo al jugador o nop
+    // --- DETECCION DEL JUGADOR  ---
+
+    public LayerMask playerLayer;    //para hacer la deteccion de solo el jugador por parte del enemigo
+    //public float rangoEnemigo;      //Distancia donde el enemigo puede detectar al jugador ...los fantasmas siguen al jugador solo si esta cerca
+    //bool enemigoActivo;             //vamos a guardar si el enemigo ya esta persiguiendo al jugador o nop
+    public float detectionRadius = 5f;     //Radio en el que el enemigo detecta al jugador
+
+    private bool playerInRange = false;     //mria si el jugador esta dentro del rango 
+
+    //PhotonView photonView;        //CAMI BORRA ESTE COMENTARIO
 
     // Se ejecuta una sola vez cuando el enemigo aparece en escena
     void Start()
     {
-        agente = GetComponent<NavMeshAgent
-        >();     //usamos la variable "agente" para decirle al enemigo a donde moverse, cual es la velocidad....
+        agent = GetComponent<NavMeshAgent>();     //usamos la variable "agente" para decirle al enemigo a donde moverse, cual es la velocidad....
     
-        dest = agente.destination;            //variable= dest la posicion actual hacia donde se esta moviendo el enemigo
+        //dest = agente.destination;            //variable= dest la posicion actual hacia donde se esta moviendo el enemigo
+
+        //photonView = GetComponent<PhotonView>();    //CAMI BORRA ESTE COMENTARIO 
         
     }
 
     // 
     void Update()
     {
-        enemigoActivo = Physics.CheckSphere(transform.position, rangoEnemigo, LayerPlayer);   //metodo checkspphere
 
-        if(enemigoActivo == true)      //entro en la espera? si si entonces..
+
+        //if (!photonView.IsMine) return;
+
+
+        //enemigoActivo = Physics.CheckSphere(transform.position, rangoEnemigo, LayerPlayer);   //metodo checkspphere
+        playerInRange = Physics.CheckSphere(transform.position, detectionRadius, playerLayer);
+
+        //if(enemigoActivo == true)      //entro en la espera? si si entonces..
+        if(playerInRange && playerTarget != null)
         {
-            if(Vector3.Distance (dest, target.position) > -1f)
+            float distance = Vector3.Distance(transform.position, playerTarget.position);
+            //if(Vector3.Distance (dest, target.position) > 1.0f)
+            if(distance >1.0f)
             {
-                dest = target.position;       //El enemigo nos empiea a perseguir porque esta buscando nuestra posicion
-                agente. destination = dest;
+                //dest = target.position;       //El enemigo nos empiea a perseguir porque esta buscando nuestra posicion
+                //agente. destination = dest;
+                agent.SetDestination(playerTarget.position);
             }
 
 
@@ -44,10 +66,16 @@ public class EnemyFollow : MonoBehaviour
         
     }
      //vamos a ver la esfera para ver como ocurre todo-Gizmos 
-        private void OnDrawGimos()
-        {
+        //private void OnDrawGimos()
+    void OnDrawGimosSelected()
+    {
+            //vamos a dibujar un circulo para mostrar el rango de deteccion
             Gizmos.color = new Color(1f, 0.5f,0f);    //Naranja intenso
-            Gizmos.DrawWireSphere(transform. position, rangoEnemigo);
-        }
+            //Gizmos.DrawWireSphere(transform. position, rangoEnemigo);
+            Gizmos.DrawWireSphere(transform. position, detectionRadius);
+    }
 
 }
+
+
+
--- Assets/Scrips/Player/PlayerMovement.cs
+++ Assets/Scrips/Player/PlayerMovement.cs
@@ -1,35 +1,61 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Photon.Pun;                                       //CAMI QUITA ESTE COMENTARIO   
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;          //velocidad la podemos cambiar en unity     
-    public float rotationSpeed = 10f;   //rotacion tambien la podemos cambiar en unity
+
+    // --- COMPONENTES Y AJUSTES PRINCIPALES --- MOVIMIENTO DEL JUGADOR
+
+
+    public float moveSpeed = 5f;          //velocidad de movimiento 
+    //public float move= 5f;   
+    public float rotationSpeed = 10f;   //rotacion maxima rota el jugador hacia la direccion de movimiento
+
+    //PhotonView photonView;                             //CAMI QUITA ESTE COMENTARIO 
 
     void Start()
     {
+        //photonView = GetComponent<PhotonView>();              //CAMI QUITA ESTE COMENTARIO
 
     }
 
-
-
-    // 
+    
     void Update()
     {
+
+        // if (!photonView.IsMine) return;    //Evita que otros jugadores cotrolen a este jugador
+
+        // --- ENTRADAS POR TECLADO FLECHAS ---
         float horizontalInput = -Input.GetAxis("Horizontal");
         float verticalInput = -Input.GetAxis("Vertical");
 
-        //Movimiento
-        Vector3 move =  new Vector3(horizontalInput, 0f, verticalInput);   //momiento en x, z
-        move.Normalize();
-        transform.position =  transform.position + move * speed * Time.deltaTime;   //deltaTime ayuda al movimiento se vea bien
+        // --- VECTOR DE MOVIMIENTO SEGUN ENTRADA DEL JUGADOR  ---
+
+        //Vector3 move =  new Vector3(horizontalInput, 0f, verticalInput);   //momiento en x, z
+        Vector3 direction= new Vector3(horizontalInput, 0f, verticalInput);
+        //move.Normalize();
+        direction.Normalize();      //Evita que el jugador se mueva mas rapido en diagonal
+
+        // --- MOVER AL PERSONAJE  ---
+
+        //transform.position =  transform.position + move * speed * Time.deltaTime;   //deltaTime ayuda al movimiento se vea bien
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
  
-        if (move != Vector3.zero)   //nos dice si el objeto se esta moviendo o nop
+        if (direction != Vector3.zero)   //nos dice si el objeto se esta moviendo o nop
         {
-            transform.rotation =  Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), rotationSpeed * Time.deltaTime);//quaterion es la funcion para los angulos
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+            //transform.rotation =  Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), rotationSpeed * Time.deltaTime);//quaterion es la funcion para los angulos
+            transform.rotation =  Quaternion.Slerp(transform.rotation,targetRotation, rotationSpeed * Time.deltaTime);
+
 
         }
         
     }
 }
+
+
